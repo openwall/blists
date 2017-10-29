99ccbcf3a96b9e28fffd74458e22626983f8d10a
ABC <abc@openwall.com>
2017-10-29 17:27:36 +0300

    Fix view message with empty filename.
    CVS-ID: html.c 1.47

--- html.c
+++ html.c
@@ -571,8 +571,8 @@ int html_message(char *list,
 						    " rel=\"nofollow\" download>Download");
 					else
 						buffer_appends(&dst, ">View");
+					buffer_appends(&dst, " attachment ");
 					if (mime.entities->filename) {
-						buffer_appends(&dst, " attachment ");
 						buffer_appends_html(&dst, mime.entities->filename);
 					}
 					buffer_appends(&dst, " of type ");
