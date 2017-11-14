41b6f8ef3e175075f397c00169ab708803e28b03
Solar Designer <solar@openwall.com>
2017-11-15 00:42:16 +0100

    Stricter command-line arguments and invocation mode (SSI or not) checking, and better error reporting.
    CVS-ID: bit.c 1.13

--- bit.c
+++ bit.c
@@ -19,10 +19,8 @@ int main(int argc, char **argv)
 	char *list, *p, nul, slash;
 	unsigned int y, m, d, n, a;
 
-	if (argc < 1 || argc > 3)
-		return html_error(NULL);
-
-	if (argc >= 2) {
+	switch (argc) {
+	case 2:
 		if (!strcmp(argv[1], "header"))
 			html_flags = HTML_HEADER;
 		else if (!strcmp(argv[1], "body"))
@@ -31,24 +29,33 @@ int main(int argc, char **argv)
 			html_flags = HTML_HEADER | HTML_CENSOR;
 		else if (!strcmp(argv[1], "body-censored"))
 			html_flags = HTML_BODY | HTML_CENSOR;
-		else if (!strcmp(argv[1], "attachment"))
+		else
+			goto bad_args;
+		break;
+	case 3:
+		if (!strcmp(argv[1], "attachment"))
 			html_flags = HTML_ATTACHMENT;
 		else
-			return html_error(NULL);
+			goto bad_args;
+		break;
+	default:
+		goto bad_args;
 	}
 
-	list = NULL;
+	p = getenv("SERVER_PROTOCOL");
+	int ssi = p && !strcmp(p, "INCLUDED");
+
 	if (html_flags != HTML_ATTACHMENT) {
-		p = getenv("SERVER_PROTOCOL");
-		if (!p || strcmp(p, "INCLUDED"))
-			return html_error(NULL);
+		if (!ssi)
+			goto bad_mode;
 		list = getenv("QUERY_STRING_UNESCAPED");
 	} else {
-		if (argc > 2)
-			list = argv[2];
+		if (ssi)
+			goto bad_mode;
+		list = argv[2];
 	}
 	if (!list)
-		return html_error(NULL);
+		goto bad_syntax;
 
 	for (p = list; *p; p++) {
 		if (p - list > 99)
@@ -64,24 +71,28 @@ int main(int argc, char **argv)
 	*p++ = '\0';
 
 	nul = '\0';
-	if (sscanf(p, "%u/%u/%u/%u/%u%c", &y, &m, &d, &n, &a, &nul) >= 5 && !nul)
-		return html_attachment(list, y, m, d, n, a);
-
+	if (html_flags == HTML_ATTACHMENT) {
+		if (sscanf(p, "%u/%u/%u/%u/%u%c", &y, &m, &d, &n, &a, &nul) >= 5 && !nul)
+			return html_attachment(list, y, m, d, n, a);
+		goto bad_syntax;
+	}
 	if (sscanf(p, "%u/%u/%u/%u%c", &y, &m, &d, &n, &nul) >= 4 && !nul)
 		return html_message(list, y, m, d, n);
-
 	if (sscanf(p, "%u/%u/%u%c%c", &y, &m, &d, &slash, &nul) >= 4 && slash == '/' && !nul)
 		return html_day_index(list, y, m, d);
-
 	if (sscanf(p, "%u/%u%c%c", &y, &m, &slash, &nul) >= 3 && slash == '/' && !nul)
 		return html_month_index(list, y, m);
-
 	if (sscanf(p, "%u%c%c", &y, &slash, &nul) >= 2 && y && slash == '/' && !nul)
 		return html_year_index(list, y);
-
 	if (!p[0])
 		return html_year_index(list, 0);
 
 bad_syntax:
 	return html_error("Invalid request syntax");
+
+bad_args:
+	return html_error("Invalid arguments");
+
+bad_mode:
+	return html_error("Invalid invocation mode");
 }
